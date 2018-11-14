00f1895987710435948e5c4ee555c4d5271dbc5f
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-11-14 18:11:16 +0100

    Add OnPropertyChanged implementation to Viewmodel to alert UI if a character is changed

--- Wanderer/ViewModel.cs
+++ Wanderer/ViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,10 +8,46 @@ using Wanderer.GameObjects;
 
 namespace Wanderer
 {
-    public class ViewModel
+    public class ViewModel : INotifyPropertyChanged
     {
-        public Hero Hero { get; set; }
-        public Character Enemy { get; set; }
+        private Hero _hero;
+        private Character _enemy;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Hero Hero
+        {
+            get { return _hero; }
+            set
+            {
+                if(_hero != value)
+                {
+                    _hero = value;
+                    OnPropertyChanged("Hero");
+                }
+            }
+        }
+        public Character Enemy
+        {
+            get { return _enemy; }
+            set
+            {
+                if(_enemy != value)
+                {
+                    _enemy = value;
+                    OnPropertyChanged("Enemy");
+                }
+            }
+        }
         public Game Game { get; set; }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
